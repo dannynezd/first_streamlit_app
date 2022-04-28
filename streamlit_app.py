ab2f65c653473d7226156155239dfbc8cbf0ee0f
dannynezd <104594657+dannynezd@users.noreply.github.com>
2022-04-28 22:50:49 +0300

    Update streamlit_app.py
    add emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.title("My Mom\'s New Healthy Dinner")
 
 streamlit.header("Brekfast menu")
 
-streamlit.text("Omega3 & Oatmeal")
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
