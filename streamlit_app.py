946a22bff7e726ce934a17bbc70a3725e55a76b7
dannynezd <104594657+dannynezd@users.noreply.github.com>
2022-04-28 22:50:08 +0300

    Update streamlit_app.py
    add more content

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,6 @@ streamlit.title("My Mom\'s New Healthy Dinner")
 streamlit.header("Brekfast menu")
 
 streamlit.text("Omega3 & Oatmeal")
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
